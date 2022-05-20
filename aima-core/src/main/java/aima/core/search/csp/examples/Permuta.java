8a1313337268523f6114e158215a66d0dacc24d0
MarceloNascc <marcelo.nasccc@gmail.com>
2022-05-20 18:36:37 -0300

    fix(Permuta): IndexOutOfBoundsException

--- aima-core/src/main/java/aima/core/search/csp/examples/Permuta.java
+++ aima-core/src/main/java/aima/core/search/csp/examples/Permuta.java
@@ -8,24 +8,25 @@ public class Permuta {
 	public static List<List<Integer>> permuta (int[] vetor, int qntd) {
 		
 		int n = vetor.length;
-		
-		
 		List<List<Integer>> values = new ArrayList<>();
-		values.add(new ArrayList<>());
-		values.get(0).addAll(Arrays.asList(4,13));
 		
 		if (qntd == 1) {
 			for(int i = 0 ; i<n ; i++) {
+				values.add(new ArrayList<>());
+
 				values.get(i).addAll(Arrays.asList(vetor[i]));
 			}
 		}
 		else if (qntd == 2) {
 			int cont = 0;
 			for(int i=0 ; i<n ; i++) {
-				for(int j=i+1 ; j<n ; j++) {
+				for(int j = i + 1 ; j<n ; j++) {
+					values.add(new ArrayList<>());
+
 					values.get(cont).addAll(Arrays.asList(vetor[i],vetor[j]));
 					cont++;
 				}
+				System.out.println("\n");
 			}
 		}
 		else if (qntd == 3) {
@@ -33,6 +34,8 @@ public class Permuta {
 			for(int i=0 ; i<n ; i++) {
 				for(int j=i+1 ; j<n ; j++) {
 					for(int k=j+1 ; k<n ; k++) {
+						values.add(new ArrayList<>());
+						
 						values.get(cont).addAll(Arrays.asList(vetor[i],vetor[j],vetor[k]));
 						cont++;
 					}
@@ -44,9 +47,12 @@ public class Permuta {
 			for(int i=0 ; i<n ; i++) {
 				for(int j=i+1 ; j<n ; j++) {
 					for(int k=j+1 ; k<n ; k++) {
-						for(int l=k+1 ; l>n ; l++)
+						for(int l=k+1 ; l<n ; l++) {
+							values.add(new ArrayList<>());
+							
 							values.get(cont).addAll(Arrays.asList(vetor[i],vetor[j],vetor[k],vetor[l]));
 							cont++;
+						}
 					}
 				}
 			}
